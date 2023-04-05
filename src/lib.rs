161f692006d83536fc2d943727a8fe6ae54df479
Trevor Manz <trevor.j.manz@gmail.com>
2023-04-05 12:03:23 -0600

    tests: add step function unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -60,8 +60,7 @@ struct NeighborhoodResult<'a> {
     labels: &'a Labels<'a>,
 }
 
-impl <'a> NeighborhoodResult<'a> {
-
+impl<'a> NeighborhoodResult<'a> {
     fn summarize(&self) -> Vec<(i32, f64)> {
         let mut data = vec![(0, 0.0); self.labels.n_categories];
         for (label, distance) in &self.distances {
@@ -299,13 +298,13 @@ impl Graph {
     }
 }
 
-
 #[pymodule]
 fn cev_metrics(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<Graph>()?;
     Ok(())
 }
 
+#[derive(Copy, Clone, Debug)]
 enum Side {
     Left,
     Right,
@@ -346,15 +345,33 @@ impl StepFunction {
         StepFunction { x, y, side }
     }
 
-    fn evaluate(&self, time: f64) -> f64 {
-        let tind = match self.side {
-            Side::Left => self.x.binary_search_by(|a| a.partial_cmp(&time).unwrap()).unwrap_or_else(|x| x),
-            Side::Right => self.x.binary_search_by(|a| a.partial_cmp(&time).unwrap()).unwrap_or_else(|x| x.saturating_sub(1)),
+    fn evaluate(&self, x: f64) -> f64 {
+        let tind = match self.x.binary_search_by(|a| a.total_cmp(&x)) {
+            Ok(i) => i - 1,
+            Err(i) => match self.side {
+                Side::Left => i - 1,
+                Side::Right => {
+                    let mut j = i;
+                    while self.x[i] == self.x[j] && j < self.x.len() - 1 {
+                        j += 1;
+                    }
+                    j
+                }
+            },
         };
-        self.y[tind]
+        self.y[clamp(tind, 0, self.y.len() - 1)]
     }
 }
 
+fn clamp<T: PartialOrd>(x: T, min: T, max: T) -> T {
+    if x < min {
+        min
+    } else if x > max {
+        max
+    } else {
+        x
+    }
+}
 
 struct ECDF {
     step_function: StepFunction,
@@ -386,11 +403,65 @@ mod tests {
     use super::*;
 
     #[test]
-    fn ecdf_example() {
-        let ecdf = ECDF::new(&vec![3., 3., 1., 4.], Side::Right);
-        let data = vec![3., 55., 0.5, 1.5];
-        let result: Vec<_> = data.iter().map(|x| ecdf.evaluate(*x)).collect();
-        // TODO: fix me!
-        assert_eq!(result, vec![0.75, 1.0, 0.0, 0.25]);
+    fn test_step_function() {
+        let x = (0..20).map(|i| i as f64).collect::<Vec<_>>();
+        let y = x.clone();
+        let f = StepFunction::new(&x, &y, 0.0, true, Side::Left);
+        let vals = [3.2, 4.5, 24., -3.1, 3.0, 4.0];
+        assert_eq!(
+            &vals
+                .iter()
+                .map(|&point| f.evaluate(point) as i32)
+                .collect::<Vec<_>>(),
+            &[3, 4, 19, 0, 2, 3],
+        )
+    }
+
+    #[test]
+    #[should_panic(expected = "x and y do not have the same length")]
+    fn test_step_function_bad_shape() {
+        let x = (0..20).map(|i| i as f64).collect::<Vec<_>>();
+        let y = (0..21).map(|i| i as f64).collect::<Vec<_>>();
+        StepFunction::new(&x, &y, 0.0, true, Side::Left);
+    }
+
+    #[test]
+    fn test_step_function_value_side_right() {
+        let x = (0..20).map(|i| i as f64).collect::<Vec<_>>();
+        let y = x.clone();
+        let f = StepFunction::new(&x, &y, 0.0, true, Side::Right);
+        let vals = [3.2, 4.5, 24., -3.1, 3.0, 4.0];
+        assert_eq!(
+            &vals
+                .iter()
+                .map(|&point| f.evaluate(point) as i32)
+                .collect::<Vec<_>>(),
+            &[3, 4, 19, 0, 3, 4],
+        )
+    }
+
+    #[test]
+    fn test_step_function_repeated_values() {
+        let x = vec![1., 1., 2., 2., 2., 3., 3., 3., 4., 5.];
+        let y = vec![6., 7., 8., 9., 10., 11., 12., 13., 14., 15.];
+        let points = vec![1., 2., 3., 4., 5.];
+
+        let f = StepFunction::new(&x, &y, 0.0, true, Side::Left);
+        assert_eq!(
+            &points
+                .iter()
+                .map(|&point| f.evaluate(point) as i32)
+                .collect::<Vec<_>>(),
+            &[0, 7, 10, 13, 14],
+        );
+
+        let f2 = StepFunction::new(&x, &y, 0.0, true, Side::Right);
+        assert_eq!(
+            &points
+                .iter()
+                .map(|&point| f2.evaluate(point) as i32)
+                .collect::<Vec<_>>(),
+            &[7, 10, 13, 14, 15],
+        );
     }
 }
